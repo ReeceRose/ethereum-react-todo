4341e7619fa86227ae22ca87c5935092141840d7
Reece Rose <reecerose18@gmail.com>
2021-11-24 23:39:08 -0500

    Now getting account id from web3

--- frontend/src/App.tsx
+++ frontend/src/App.tsx
@@ -2,20 +2,27 @@ import { Component } from "react";
 import Web3 from "web3";
 import "./App.css";
 
-class App extends Component {
+interface State {
+  account: string;
+}
+
+class App extends Component<{}, State> {
   componentDidMount() {
     this.loadBlockchainData();
   }
 
   constructor(props: any) {
     super(props);
-    console.log("started");
+    this.state = { account: "" };
   }
 
   async loadBlockchainData() {
     const web3 = new Web3(Web3.givenProvider || "http://localhost:7545");
     const network = await web3.eth.net.getNetworkType();
+    let accounts = await web3.eth.requestAccounts();
+    this.setState({ account: accounts[0] });
     console.log(network);
+    console.log(accounts[0]);
   }
 
   render() {
@@ -23,6 +30,7 @@ class App extends Component {
       <div className="App">
         <div className="App-content">
           <h1 className="App-header">Ethereum + React Todo List</h1>
+          <h6>Your account: {this.state.account}</h6>
           <ul id="taskList" className="list-unstyled">
             <div className="tasktemplate checkbox display-none">
               <label>
