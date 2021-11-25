0acec1bf423ca80a820f26e16a6443a006b085da
Reece Rose <reecerose18@gmail.com>
2021-11-24 22:46:57 -0500

    More work on todolist contract

--- constracts/contracts/TodoList.sol
+++ constracts/contracts/TodoList.sol
@@ -2,4 +2,22 @@ pragma solidity ^0.8.10;
 
 contract TodoList {
   uint public taskCount = 0;
+
+  struct Task {
+    uint id;
+    string content;
+    bool completed;
+  }
+
+  constructor() {
+    // seed blockchain
+    createTask("Finish this project");
+  }
+
+  mapping(uint => Task) public tasks;
+
+  function createTask(string memory _content) public {
+    taskCount++;
+    tasks[taskCount] = Task(taskCount, _content, false);
+  }
 }
